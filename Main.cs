392924389dedac80e670964d71b3b40c04b12259
Balkoth-dev <webmastaman@yahoo.com>
2021-11-08 21:22:27 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityModManagerNet;
+
+namespace WOTR_PATH_OF_RAGE
+{
+    public class Main
+    {
+        public static UnityModManager.ModEntry modInfo = null;
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            modInfo = modEntry;
+            harmony.PatchAll();
+            return true;
+        }
+        public static void Log(string msg)
+        {
+            modInfo.Logger.Log(msg);
+        }
+    }
+}
---
+++ MechanicsChanges/HasAnyFlag.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace WOTR_PATH_OF_RAGE.MechanicsChanges
+{
+    class HasAnyFlagFix
+    {
+        [HarmonyPatch(typeof(SpellDescriptorOperations), "HasAnyFlag")]
+        class Patch
+        {
+            static void Postfix(ref bool __result)
+            {
+                if (__result == false)
+                {
+                    __result = true;
+                }
+                Main.Log("PostFix");
+            }
+        }
+
+        [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+        class Patch2
+        {
+            static void Postfix()
+            {
+                Main.Log("BLUBBBLES");
+            }
+        }
+    }
+}
